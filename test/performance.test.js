9fe7327c7f2e632b0a6d4cd2ccc66057d6be0d88
DanMHammer <35697323+DanMHammer@users.noreply.github.com>
2021-07-28 22:09:41 -0500

    Improve performance by not importing the source document repeatedly in for loops

---
+++ test/performance.test.js
@@ -0,0 +1,75 @@
+const path = require('path')
+const fs = require('fs-extra')
+
+const PDFMerger = require('../index')
+const PDFMergerBrowser = require(`../browser`)
+
+const FIXTURES_DIR = path.join(__dirname, 'fixtures')
+const TMP_DIR = path.join(__dirname, 'tmp')
+
+jest.setTimeout(100000)
+
+const output = [];
+
+describe(`performance issues`, () => {
+  describe(`memory and time issues`, () => {
+    let startTime = process.hrtime()
+    let maxMemoryConsumption = 0
+
+    beforeAll(async () => {
+      await fs.ensureDir(TMP_DIR)
+      global.gc()
+      await new Promise(resolve => setTimeout(resolve, 1000))
+    });
+
+    beforeEach(async () => {
+      startTime = process.hrtime();
+      process.nextTick(() => {
+        let memUsage = process.memoryUsage();
+        if (memUsage.rss > maxMemoryConsumption) {
+          maxMemoryConsumption = memUsage.rss;
+        }
+      })
+    })
+
+    afterEach(async () => {
+      const time = process.hrtime(startTime)
+      output.push({name: expect.getState().currentTestName, time: `${time[0]}.${time[1]} s`, memory: `${maxMemoryConsumption / (1024 * 1024)} MB`})
+      maxMemoryConsumption = 0
+      global.gc()
+      await new Promise(resolve => setTimeout(resolve, 1000))
+    })
+
+    test(`original: Merging several PDFs takes a long time/Uses a lot of memory (#27)`, async () => {
+      const merger = new PDFMerger()
+      
+      merger.add(path.join(FIXTURES_DIR, 'apollo_17.pdf'), Array.from(Array(20), (_, index) => index + 1))
+      merger.add(path.join(FIXTURES_DIR, 'sense_and_sensibility.pdf'), "1 - 254")
+      merger.add(path.join(FIXTURES_DIR, 'pride_and_prejudice.pdf'), "1 to 264")
+      merger.add(path.join(FIXTURES_DIR, 'long_wikipedia_page.pdf'), "1 to 100")
+
+      await merger.save(path.join(TMP_DIR, 'output1.pdf'))
+    })
+
+    test(`browser: Merging several PDFs takes a long time/Uses a lot of memory (#27)`, async () => {
+      const merger = new PDFMergerBrowser()
+
+      await merger.add(await fs.readFile(path.join(FIXTURES_DIR, 'apollo_17.pdf')), Array.from(Array(20), (_, index) => index + 1))
+      await merger.add(await fs.readFile(path.join(FIXTURES_DIR, 'sense_and_sensibility.pdf')), "1 - 254")
+      await merger.add(await fs.readFile(path.join(FIXTURES_DIR, 'pride_and_prejudice.pdf')), "1 to 264")
+      await merger.add(await fs.readFile(path.join(FIXTURES_DIR, 'long_wikipedia_page.pdf')), "1 to 100")
+      
+      const buffer = await merger.saveAsBuffer()
+
+      // Write the buffer as a file
+      await fs.writeFile(path.join(TMP_DIR, "outputx.pdf"), buffer)
+    })
+
+  })
+
+  afterAll(async () => {
+    await fs.remove(TMP_DIR)
+    console.log(output)
+    global.gc()
+  })
+})
